efde9a738e0461bab256cd31ca83bae7c1e5195f
misakano7545 <misakano7545@protonmail.com>
2022-04-30 17:18:10 +0800

    Change import repo

--- app/mydispatcher/default.go
+++ app/mydispatcher/default.go
@@ -9,8 +9,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/XrayR-project/XrayR/common/limiter"
-	"github.com/XrayR-project/XrayR/common/rule"
+	"github.com/Misaka-blog/XrayR/common/limiter"
+	"github.com/Misaka-blog/XrayR/common/rule"
 	"github.com/xtls/xray-core/common"
 	"github.com/xtls/xray-core/common/buf"
 	"github.com/xtls/xray-core/common/log"
